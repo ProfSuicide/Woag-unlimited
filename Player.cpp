1c49b89b9d3a5f88ecf8557e0ade6577ccd98321
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-12 12:03:50 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,3 +2,15 @@
 #include <string>
 #include "Player.h"
 
+Player::Player(){
+  name = "No Name";
+  health = 10;
+}
+
+Player::setName(string userName){
+  name = userName;
+}
+Player::setHealth(int userHealth){
+  health = userHealth;
+}
+
