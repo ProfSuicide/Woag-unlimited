c5f2b82b473f1145d6ddb4e38d1e41ab6537675d
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-12 12:19:02 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,15 +2,34 @@
 #include <string>
 #include "Player.h"
 
-Player::Player(){
+Player::Player(){ // default constructor 
   name = "No Name";
   health = 10;
 }
-
-Player::setName(string userName){
+Player::setName(string userName){ 
   name = userName;
 }
 Player::setHealth(int userHealth){
   health = userHealth;
 }
+Player::setWeapon(int userWeapon){
+  currentWeapon = userWeapon;
+}
+Player::setDamage(int userDamage){
+  weaponDamage = userDamage;
+}
 
+
+
+Player::getName() const{
+  return name;
+}
+Player::getHealth() const{
+  return health;
+}
+Player::getWeapon() const{
+  return currentWeapon;
+}
+Player::getDamage() const{
+  return weaponDamage;
+}
