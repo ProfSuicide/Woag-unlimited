0194be4d47f5e0858676ea39af8149adc86467ff
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-12 22:55:17 -0800

    Update project4.cpp

--- project4.cpp
+++ project4.cpp
@@ -4,13 +4,19 @@
 //Course: CompSci 1
 //Description:  This program will 
 //Usage:  Entertainment
+
 #include <iostream>
 #include <string>
 #include <time.h>
 #include <cstlib>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
 using namespace std;
 using namespace sf;
+
 int main()
 {
+  
+  return 0;
 }
