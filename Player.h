10d8b38c23b90aefcc5ae2fb4e7f945fb53fd875
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-12 22:53:21 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -10,9 +10,13 @@ using namespace sf; // this is for sfml
 class Player{
   public:
     void setName(string userName);
-    void setHealth(int userHealth); // between 1-100 
-    void setWeapon(int userWeapon);
-    void setDamage(int damageAmount);
+    void setHealth(int userHealth);   // between 1-100 
+    void setWeapon(int userWeapon);   
+    void setDamage(int damageAmount); 
+    string getName() const;           // returns name
+    int getHealth() const;            // returns health 
+    int getWeapon() const;            // returns currentWeapon 
+    int getDamage() const;            // returns weaponDamage
     string inventory[3];
   private:
     string name;
