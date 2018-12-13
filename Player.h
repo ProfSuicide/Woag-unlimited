8053d352f37e07cc6f9f04b7888691902820bd69
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-12 17:25:14 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,26 +1,23 @@
-#ifndef PLAYER_H
+#ifndef PLAYER_H // header guard 
 #define PLAYER_H
 
 #include <iostream>
 #include <string>
+
 using namespace std;
-using namespace sf;
+using namespace sf; // this is for sfml 
 
 class Player{
   public:
-  void setName(string userName);
-  void setHealth(int userHealth);
-  void setWeapon(int userWeapon);
-  void setDamage(int userDamage);
-  string inventory[3];
+    void setName(string userName);
+    void setHealth(int userHealth); // between 1-100 
+    void setWeapon(int userWeapon);
+    void setDamage(int damageAmount);
+    string inventory[3];
   private:
-  string name;
-  int health;
-  int currentWeapon;
-  int weaponDamage;
-  
-  
-};
-
-  
+    string name;
+    int health;
+    int currentWeapon;
+    int weaponDamage; 
+};  
 #endif
