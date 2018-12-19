ad1eba6303bcfe86015baa5cfe739b111c055081
ProfSuicide <45830910+ProfSuicide@users.noreply.github.com>
2018-12-18 20:57:17 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,27 +23,15 @@ void print(string str) // print function to output text with a time delay for ea
     }
 }
 
-void playerQuestAccepted(){
-  cout << "Oh thank you so much helping me! \nYou should take a look around town before you head out on your journey!" << endl;
-}
-void playerQuestDenied(){
-  cout << "You will save the princess for me! \nYou will fight the sorcerer and you will either succeed or die trying!" << endl;
-}
-
-void weaponsmithIntro(){
-  cout << "Aye! I’ve heard the good news. You’re here to help us save the princess. "<< endl;
-  cout << "My name is Grimbow and I can’t begin to tell you how grateful we are for \nyou to help us get this Dagan the Duke out of our town!" << endl;
-  cout << "He strolls into town a fortnite ago and demands free food and shelter along with help saving the princess. " << endl;
-  
-}
-
 int main()
 {
+  srand (time(NULL));
   const int weaponSize=3;
   int weapon[weaponSize];
   weapon[0]=0;
   weapon[1]=0;
   weapon[2]=0;
+  int weaponland=rand() % 20 + 1;
   
   return 0;
 }
