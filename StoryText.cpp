9043e2bad001988400af72e6d2d8ce5b7da3845a
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-18 19:23:42 -0800

    Create StoryText.cpp

---
+++ StoryText.cpp
@@ -0,0 +1,43 @@
+#include "StoryText.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+StoryText::StoryText()
+{
+    file = "blank";
+}
+void StoryText::readFile(string fileName)
+{
+    ifstream storyText;
+    storyText.open(fileName);
+
+    if(!storyText.is_open()) // closes the window if the file cannot be opened
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    char word[500];   // if the file can be opened this will execute
+    storyText >> word;
+    while(storyText.good())
+    {
+        print(word);
+        cout << " ";
+        storyText >> word;
+    }
+    Sleep(5000);   // time delay for user to finish reading the text file
+    system("cls"); // clears screen
+
+}
+void StoryText::print(string str) // print function to output text with a time delay for each character
+{
+    for (int i = 0; i < str.size(); ++i)
+    {
+        cout << str[i];
+        Sleep(5); // feel free to change this if you think it's too slow or too fast
+    }
+}
