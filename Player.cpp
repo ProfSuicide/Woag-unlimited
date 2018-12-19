6fadf071342f0576ff74881b46accf83afe6bb12
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-18 19:35:14 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,37 +1,68 @@
+#include "Player.h"
 #include <iostream>
 #include <string>
-#include "Player.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-Player::Player(){ // default constructor 
-  name = "stranger";
-  health = 100;
+Player::Player() // default constructor
+{
+    name = "stranger";
+    health = 100;
 }
-void Player::setName(string userName){ 
-  name = userName;
+Player::Player(string userName, int userHealth) // overloaded constructor
+{
+    name = userName;
+    health = userHealth;
 }
-void Player::setHealth(int userHealth){
-  health = userHealth;
+void Player::setName(string userName)
+{
+    name = userName;
 }
-void Player::setWeapon(int userWeapon){
-  currentWeapon = userWeapon;
+void Player::setResponse(string userResponse)
+{
+    response = userResponse;
 }
-void Player::setDamage(int damageAmount){ // damage from player's weapon 
-  weaponDamage = damageAmount;
+void Player::setHealth(int userHealth)
+{
+    health = userHealth;
 }
+void Player::setWeapon(int userWeapon)
+{
+    currentWeapon = userWeapon;
+}
+void Player::setDamage(int damageAmount) // damage from player's weapon
+{
+    weaponDamage = damageAmount;
+}
+void Player::playerRoll() // generates two random dice rolls between 1-20
+{
+    char x  = '1';
+    srand(time(0));
 
-
-
-string Player::getName() const{
-  return name;
+    for(int i = 0; i < 2; ++i){
+        int diceRoll = rand() % 20 + 1;
+        cout <<"Roll number " << x << " was a " << diceRoll;
+        cout << endl << endl;
+        x++;
+    }
+}
+//--------- getter functions ------------
+string Player::getName() const
+{
+    return name;
+}
+string Player::getResponse() const
+{
+    return response;
 }
 int Player::getHealth() const{
-  return health;
+    return health;
 }
 int Player::getWeapon() const{
-  return currentWeapon;
+    return currentWeapon;
 }
 int Player::getDamage() const{
-  return weaponDamage;
+    return weaponDamage;
 }
