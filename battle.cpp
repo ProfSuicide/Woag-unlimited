565f693251e462555ac322505812b425f94efc76
ProfSuicide <45830910+ProfSuicide@users.noreply.github.com>
2018-12-19 17:56:37 -0800

    Update battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -5,4 +5,18 @@ if(battleTurn==1){
 cout << "The Sorcerer attacks! << endl;
 enemyLand=rand() % 20 + 1;
 if(enemyLand==20){
+  cout << "The Sorcerer lands a critical!" << endl;
+  enemyDamage=(rand() % 10 + 4)*2;
+}
+if((enemyLand>=10) && (enemyLand<=19){
+  cout << "The Sorcerer lands a hit!" << endl;
+  enemyDamage=rand() % 10 + 4;
+}
+if((enemyLand=<9) && (enemyLand>=1)){
+  cout << "The Sorcerer misses!" << endl;
+}
+if(enemyLand==1){
+  
+   
+  
 
