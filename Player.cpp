4cc0791f29f3dc8cd2393f0ddeeaf3781efd25a8
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-30 20:41:39 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,55 +3,33 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
+
 
 Player::Player() // default constructor
 {
     name = "stranger";
     health = 100;
 }
-Player::Player(string userName, int userHealth) // overloaded constructor
+Player::Player(string userName, int userHealth)
 {
     name = userName;
     health = userHealth;
 }
-//--------- setter functions ------------
-void Player::setName(string userName)
-{
+void Player::setName(string userName){
     name = userName;
 }
-void Player::setResponse(string userResponse)
-{
-    response = userResponse;
-}
-void Player::setHealth(int updateHealth){ // updates player's health, updateHealth parameter takes in damage amount as an argument
+
+void Player::setHealth(int updateHealth){ // updates player's health, updateHealth parameter is the damage amount
     health -= updateHealth;
 }
-void Player::setWeapon(int userWeapon)
-{
-    currentWeapon = userWeapon;
-}
-void Player::setDamage(int damageAmount) // damage from player's weapon
-{
-    weaponDamage = damageAmount;
-}
-
-//--------- getter functions ------------
+//-------- getter functions ------------
 string Player::getName() const
 {
     return name;
 }
-string Player::getResponse() const
-{
-    return response;
-}
 int Player::getHealth() const{
     return health;
-}
-int Player::getWeapon() const{
-    return currentWeapon;
-}
-int Player::getDamage() const{
-    return weaponDamage;
 }
