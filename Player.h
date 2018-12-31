9b20934cae3fc30a08a661fcd8a0b70ff43f74b8
lizard-grandpa <44102531+lizard-grandpa@users.noreply.github.com>
2018-12-30 20:42:42 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -8,27 +8,21 @@
 
 using namespace std;
 
+
 class Player
 {
     public:
         Player();                                // default constructor
         Player(string userName, int userHealth); // overloaded constructor
         void setName(string userName);
-        void setResponse(string userResponse);   // stores any answers from the user
-        void setHealth(int updateHealth);        // updates health when player takes damage
-        void setWeapon(int userWeapon);
-        void setDamage(int damageAmount);
-        string getName() const;              // returns name
-        string getResponse() const;          // returns response
-        int getHealth() const;               // returns health
-        int getWeapon() const;               // returns currentWeapon
-        int getDamage() const;               // returns weaponDamage
+        void setHealth(int userHealth);          // between 1-100
+        void print();
+        string getName() const;                  // returns name
+        int getHealth() const;                   // returns health
+        string inventory[3];
     private:
         string name;
-        string response;
         int health;
-        int currentWeapon;
-        int weaponDamage;
 };
 
 #endif // PLAYER_H
